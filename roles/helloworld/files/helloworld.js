54bcb1f032f75265e0119d63082b4194904ddf94
ericwudayi <r07922119@ntu.edu.tw>
2021-12-06 15:54:36 +0800

    change content

--- roles/helloworld/files/helloworld.js
+++ roles/helloworld/files/helloworld.js
@@ -8,7 +8,7 @@ http.createServer(function (request, response) {
    response.writeHead(200, {'Content-Type': 'text/plain'})
 
    // Send the response body as "Hello World"
-   response.end('Hello World, and fuck!\n')
+   response.end('Hello World, and fuck anss!\n')
 }).listen(3000)
 
 // Console will print the message
